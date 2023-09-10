23f19dcea1742847a0531529aac49a1bfb267a61
ading2210 <ading2019@gmail.com>
2023-09-10 02:29:35 -0400

    account creation working

--- src/freedns/__init__.py
+++ src/freedns/__init__.py
@@ -1,1 +1,1 @@
-print("hello world")
+from .client import Client
---
+++ src/freedns/client.py
@@ -0,0 +1,36 @@
+import requests
+
+BASE_URL = "https://freedns.afraid.org"
+
+class Client:
+  def __init__(self):
+    self.headers = {
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+    self.session = requests.Session() 
+    self.session.headers.update(self.headers)
+
+  def get_captcha(self):
+    captcha_url = BASE_URL+"/securimage/securimage_show.php"
+    response = self.session.get(captcha_url)
+    return response.content
+  
+  def create_account(self, firstname, lastname, username, password, email, captcha_code):
+    account_create_url = BASE_URL+"/signup/?step=2"
+    payload = {
+      "plan": "starter",
+      "firstname": firstname,
+      "lastname": lastname, 
+      "username": username,
+      "password": password,
+      "password2": password,
+      "email": email,
+      "captcha_code": captcha_code,
+      "tos": "1",
+      "PROCID": "",
+      "TRANSPRE": "",
+      "action": "signup",
+      "send": "Send+activation+email"
+    }
+
+    self.session.post(account_create_url, data=payload)
