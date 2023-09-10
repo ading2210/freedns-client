7ba1ccec54d3cced060e812622b5b0d1e7e5be20
ading2210 <ading2019@gmail.com>
2023-09-10 01:57:18 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools, glob
+from pathlib import Path
+
+#note: build this package with the following command:
+#pip wheel --no-deps -w dist .
+
+base_path = Path(__file__).parent
+long_description = (base_path / "README.md").read_text()
+
+setuptools.setup(
+  name="freedns-client",
+  version="0.0.1",
+  author="ading2210",
+  license="GPLv3",
+  description="A Python package for interacting with FreeDNS.afraid.org",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  packages=["freedns"],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Operating System :: OS Independent"
+  ],
+  python_requires=">=3.7",
+  package_dir={
+    "": "src"
+  },
+  package_data={},
+  include_package_data=True,
+  install_requires=[
+    "requests[socks]",
+    "pysocks" #proxy support to be added later
+  ],
+  url="https://github.com/ading2210/freedns-client"
+)
